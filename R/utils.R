13e791e010be7e5255a553b101932eaade200746
Thomas Jc <thomas.jcng@gmail.com>
2018-08-20 17:06:36 +0800

    Support batch KVs

--- R/utils.R
+++ R/utils.R
@@ -6,9 +6,12 @@ check_status <- function(res){
 }
 
 
-#' Get a single key/value from Consul
+#' Obtain key/value from Consul
 #'
-#' @param key which key to retrieve
+#' @param key Key name to retrieve
+#' @param path Path name to retrieve
+#' @param ... One or many keys under path to retrieve.
+#' All but path name will be coersed into a list.
 #'
 #' @return
 #' Decoded value retrieved from Consul. 'NA' if empty.
@@ -17,10 +20,19 @@ check_status <- function(res){
 #' @importFrom httr GET content
 #' @importFrom base64enc base64decode
 #' @export
+#' @rdname get_kv
 #'
 #' @examples
 #' \dontrun{
+#'
+#' # Return a string
 #' get_kv("fruit/apple")
+#'
+#' # Return a list of values
+#' get_batch_kv("fruit", c("apple", "banana", "coconut"))
+#' # This works too
+#' get_batch_kv("fruit", "apple", "banana", "coconut")
+#'
 #' }
 #'
 get_kv <- function(key) {
@@ -52,4 +64,61 @@ get_kv <- function(key) {
 
 }
 
+#' @export
+#' @rdname get_kv
+get_batch_kv <- function(path, ...) {
 
+    # required paramters
+    params <- c("host", "port", "swagger")
+
+    # defensive
+    if(!all(params %in% ls(envir = consul))) {
+        stop("One or more Consul parameters cannot be found.")
+    }
+
+    # forming url
+    url <- sprintf("http://%s:%s/%s/%s?recurse", consul$host, consul$port, consul$swagger, path)
+
+    # send GET request
+    res <- GET(url) %>% content()
+
+    # if return empty
+    if (!length(res) > 0) {
+        return(NULL)
+    }
+
+    # first on list is folder/, not useful
+    res[[1]] <- NULL
+
+    # from Consul
+    consul.k <- sapply(res, function(x) x[["Key"]])
+
+    # from user
+    user.k <- sapply(list(...), function(x) paste0(path, "/", x))
+
+    # extract value based on keys specified by user
+    encoded_vals <- sapply(user.k, function(x) {
+        tryCatch(
+            res[[which(consul.k == x)]]["Value"],
+            error = function(e) "JQ=="
+        )
+    })
+
+    # decode
+    vals <- lapply(encoded_vals, function(x) {
+        if(!is.na(x)) {
+            tryCatch(
+                rawToChar(base64decode(x)),
+                error = function(e) NA
+            )}
+    })
+
+    # format names of list
+    names(vals) <- names(vals) %>%
+        gsub(pattern = "^\\w*/", replacement = "") %>%
+        gsub(pattern = "\\.\\w+", replacement = "")
+
+    # return
+    vals
+
+}
